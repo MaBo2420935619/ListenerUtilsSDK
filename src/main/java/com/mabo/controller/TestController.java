82234dadcaa1aaaac622295744684181b253b573
mabo_9704 <mabo_9704@163.com>
2022-07-18 01:01:01 +0800

    修改sdk，增加log日志

--- src/main/java/com/mabo/controller/TestController.java
+++ src/main/java/com/mabo/controller/TestController.java
@@ -1,17 +1,22 @@
 package com.mabo.controller;
 
 import com.mabo.event.Event;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
+    private  static Logger log = LoggerFactory.getLogger(TestController.class);
+
     //浏览器输入
     //http://localhost:8081/event
     @Autowired
     private Event event;
     @RequestMapping("event")
     public String request(){
+        log.info("开始event");
         String event = this.event.event("测试数据");
         return event;
     }
--- src/main/java/com/mabo/listener/EventListener.java
+++ src/main/java/com/mabo/listener/EventListener.java
@@ -1,18 +1,23 @@
 package com.mabo.listener;
 
+import com.mabo.event.Event;
 import com.mabo.utils.AddEventListener;
 import com.mabo.utils.EventType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Component
 public class EventListener {
+    private  static Logger log = LoggerFactory.getLogger(EventListener.class);
+
     @AddEventListener(value = "eventId",position = EventType.AFTEREVENT)
     public void test1(String s) {
-        System.out.println("test1执行成功，参数:"+s);
+        log.info("test1执行成功，参数:"+s);
     }
 
     @AddEventListener(value = "reduce")
     public void test2(String s) {
-        System.out.println("test2执行成功，参数:"+s);
+        log.info("test2执行成功，参数:"+s);
     }
 }
