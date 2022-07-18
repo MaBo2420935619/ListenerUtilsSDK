d46ace7273acb778e6cbd35ce4217c8bec4332a4
mabo <mabo_9704@163>
2022-07-18 14:03:15 +0800

    修改

--- src/main/java/com/mabo/ListenerApplication.java
+++ src/main/java/com/mabo/ListenerApplication.java
@@ -1,8 +1,9 @@
 package com.mabo;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@MapperScan("com.mabo.dao")
 @SpringBootApplication
 public class ListenerApplication {
 
